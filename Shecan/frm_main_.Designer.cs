d0795e2597ef81438478eada054c36f33691e503
kasbarg <generalneverhood>
2019-06-24 00:13:44 +0430

    First Push
    Upload Source

---
+++ Shecan/frm_main_.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Shecan
+{
+    partial class frm_main_
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_main_));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.اطلاعاتبیشترToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.دربارهسرویسشکنToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.دربارهسازندهابزارToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(251, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "فعال سازی";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(251, 96);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "غیر فعال سازی";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LavenderBlush;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 10F);
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(251, 148);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(225, 46);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "خروج";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Shecan.Properties.Resources.shecan;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(27, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(189, 154);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.اطلاعاتبیشترToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(509, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // اطلاعاتبیشترToolStripMenuItem
+            // 
+            this.اطلاعاتبیشترToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.دربارهسرویسشکنToolStripMenuItem,
+            this.دربارهسازندهابزارToolStripMenuItem});
+            this.اطلاعاتبیشترToolStripMenuItem.Name = "اطلاعاتبیشترToolStripMenuItem";
+            this.اطلاعاتبیشترToolStripMenuItem.Size = new System.Drawing.Size(90, 20);
+            this.اطلاعاتبیشترToolStripMenuItem.Text = "اطلاعات بیشتر";
+            // 
+            // دربارهسرویسشکنToolStripMenuItem
+            // 
+            this.دربارهسرویسشکنToolStripMenuItem.Name = "دربارهسرویسشکنToolStripMenuItem";
+            this.دربارهسرویسشکنToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.دربارهسرویسشکنToolStripMenuItem.Text = "درباره سرویس شکن";
+            this.دربارهسرویسشکنToolStripMenuItem.Click += new System.EventHandler(this.دربارهسرویسشکنToolStripMenuItem_Click);
+            // 
+            // دربارهسازندهابزارToolStripMenuItem
+            // 
+            this.دربارهسازندهابزارToolStripMenuItem.Name = "دربارهسازندهابزارToolStripMenuItem";
+            this.دربارهسازندهابزارToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.دربارهسازندهابزارToolStripMenuItem.Text = "درباره سازنده ابزار";
+            this.دربارهسازندهابزارToolStripMenuItem.Click += new System.EventHandler(this.دربارهسازندهابزارToolStripMenuItem_Click);
+            // 
+            // frm_main_
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(509, 229);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frm_main_";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "تنظیم شکن (نسخه غیررسمی)";
+            this.Load += new System.EventHandler(this.frm_main__Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem اطلاعاتبیشترToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem دربارهسرویسشکنToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem دربارهسازندهابزارToolStripMenuItem;
+    }
+}
+
---
+++ Shecan/frm_main_.cs
@@ -0,0 +1,167 @@
+﻿/*
+    ابزار استفاده از سرویس شکن
+    (سرویس دور زدن تحریم های اینترنتی)
+    
+    Service Website: http://Shecan.ir
+    UnOfficial Client Website: http://AlphaTech-Group.ir
+
+
+Reference : https://stackoverflow.com/questions/40291375/how-to-change-dns-with-c-sharp-on-windows-10
+*/
+using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+using System.Management;
+using System.Security.Principal;
+using System.Net;
+
+
+namespace Shecan
+{
+    public partial class frm_main_ : Form
+    {
+        public frm_main_()
+        {
+            InitializeComponent();
+        }
+        public bool IsUserAdministrator()
+            {
+                bool isAdmin;
+                try
+                {
+                    WindowsIdentity user = WindowsIdentity.GetCurrent();
+                    WindowsPrincipal principal = new WindowsPrincipal(user);
+                    isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    isAdmin = false;
+
+                }
+                catch (Exception ex)
+                {
+                    isAdmin = false;
+                }
+                return isAdmin;
+            }
+
+    public  NetworkInterface GetActiveEthernetOrWifiNetworkInterface()
+        {
+            var Nic = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault(
+                a => a.OperationalStatus == OperationalStatus.Up &&
+                (a.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || a.NetworkInterfaceType == NetworkInterfaceType.Ethernet) &&
+                a.GetIPProperties().GatewayAddresses.Any(g => g.Address.AddressFamily.ToString() == "InterNetwork"));
+
+            return Nic;
+        }
+        public void SetDNS(string[] DnsString)
+        {
+            string[] Dns =  DnsString ;
+            var CurrentInterface = GetActiveEthernetOrWifiNetworkInterface();
+            if (CurrentInterface == null) return;
+
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    if (objMO["Caption"].ToString().Contains(CurrentInterface.Description))
+                    {
+                        ManagementBaseObject objdns = objMO.GetMethodParameters("SetDNSServerSearchOrder");
+                        if (objdns != null)
+                        {
+                            objdns["DNSServerSearchOrder"] = Dns;
+                            objMO.InvokeMethod("SetDNSServerSearchOrder", objdns, null);
+                        }
+                    }
+                }
+            }
+        }
+        public  void UnsetDNS()
+        {
+            var CurrentInterface = GetActiveEthernetOrWifiNetworkInterface();
+            if (CurrentInterface == null) return;
+
+            ManagementClass objMC = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection objMOC = objMC.GetInstances();
+            foreach (ManagementObject objMO in objMOC)
+            {
+                if ((bool)objMO["IPEnabled"])
+                {
+                    if (objMO["Caption"].ToString().Contains(CurrentInterface.Description))
+                    {
+                        ManagementBaseObject objdns = objMO.GetMethodParameters("SetDNSServerSearchOrder");
+                        if (objdns != null)
+                        {
+                            objdns["DNSServerSearchOrder"] = null;
+                            objMO.InvokeMethod("SetDNSServerSearchOrder", objdns, null);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var ips = Dns.GetHostEntry("dns.shecan.ir").AddressList;
+                string[] DNSs= { ips[0].ToString(), ips[1].ToString() };
+                SetDNS(DNSs);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("فعال سازی با مشکل مواجه شد", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            MessageBox.Show("فعال سازی با موفقیت انجام شد", "فعال شد", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UnsetDNS();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("غیرفعال سازی با مشکل مواجه شد","خطا",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+            MessageBox.Show("غیرفعال سازی با موفقیت انجام شد", "غیرفعال شد", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+
+        private void frm_main__Load(object sender, EventArgs e)
+        {
+            if (!IsUserAdministrator())
+            {
+                MessageBox.Show("شما باید با دسترسی مدیر سیستم از این نرم افزار استفاده کنید", "دسترسی محدود", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                Application.Exit();
+            }
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://shecan.ir");
+        }
+
+        private void دربارهسرویسشکنToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://shecan.ir");
+        }
+
+        private void دربارهسازندهابزارToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://alphatech-group.ir");
+        }
+    }
+}
